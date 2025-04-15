c14594b6c97ca673e4e6c05b7a54c9bd4c2dea26
Patrycja Pawlicka <pawlicka.patrycja@yahoo.com>
2025-04-15 05:09:48 +0100

    Playing around with JavaScript

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,19 @@
+// alert("Hello, World! I am an alert box!");
+
+// var student = {
+//     firstName: "Jane",
+//     lastName: "Smith",
+//     age: 28,
+//     gradeAverage: 3.5
+// };
+
+// var student = {
+//     firstName: "Jane",
+//     lastName: "Smith",
+//     age: 28,
+//     gradeAverage: function(avg) {
+//         return avg;
+//     }
+// };
+
+
